389bb38de33652e63419523a50ed7d1f2ff31aa5
kimhanui <cocacola2017@naver.com>
2022-07-05 23:48:10 +0900

    fix : char에서 'a'대신 '0' 소거

--- kimhanui/src/test/java/baseballgame/BaseballGame.java
+++ kimhanui/src/test/java/baseballgame/BaseballGame.java
@@ -11,18 +11,13 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class BaseballGame {
 
-    //11:02
-    //상대받(컴퓨터)의 역할을 짜면 될 듯 함
-    private final String STRIKE = "스트라이크";
-    private final String BALL = "볼";
-
     @ParameterizedTest
-    @CsvSource(value={"123:123:3"}, delimiter=':')
+    @CsvSource(value={"123:123:3", "123:222:1", "123:341:0"}, delimiter=':')
     void isStrikeOrNotTest(String answer, String tried, String expected){
         int strikeCnt = 0;
         for(int i=0;i<3;i++){
-            int n1 = answer.charAt(i)-'a';
-            int n2 = tried.charAt(i)-'a';
+            int n1 = answer.charAt(i)-'0';
+            int n2 = tried.charAt(i)-'0';
             strikeCnt += cntStrike(n1, n2);
         }
         assertThat(strikeCnt).isEqualTo(parseInt(expected));
