03237e2a128a35c29185f53b85b5658512a13cf7
riyenas0925 <riyenas0925@gmail.com>
2022-07-10 20:08:16 +0900

    feat: 심판은 같은 수가 같은 자리에 있으면 스트라이크임을 알려줘야 한다.

--- riyenas0925/src/main/java/Referee.java
+++ riyenas0925/src/main/java/Referee.java
@@ -8,6 +8,18 @@ public class Referee {
         this.answer = answer;
     }
 
+    public int countStrike(List<Integer> guess) {
+        int cnt = 0;
+
+        for(int i = 0; i < 3; i++) {
+            if(guess.get(i) == answer.get(i)) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
     private void validateNumber(List<Integer> number) {
         if(number.size() != 3)
             throw new IllegalArgumentException("입력값은 3자리여야 합니다.");
--- riyenas0925/src/test/java/RefereeTest.java
+++ riyenas0925/src/test/java/RefereeTest.java
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -29,4 +32,30 @@ public class RefereeTest {
                 arguments(Arrays.asList(1, 2))
         );
     }
+
+    @DisplayName("심판은 같은 수가 같은 자리에 있으면 스트라이크임을 알려줘야 한다.")
+    @ParameterizedTest(name = "실제 값: {1}, 추측 값: {2}, 스트라이크 개수: {0}")
+    @CsvSource(value = {
+            "0,123,234",
+            "1,123,134",
+            "2,123,124",
+            "3,123,123"
+    }, delimiter = ',')
+    void countStrike(int actual, String answer, String guess) {
+        // given
+        Referee referee = new Referee(split(answer));
+
+        // when
+        int expected = referee.countStrike(split(guess));
+
+        // that
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private List<Integer> split(String number) {
+        return number.chars().boxed()
+                .map(e -> e - '0')
+                .collect(Collectors.toList());
+    }
+
 }
