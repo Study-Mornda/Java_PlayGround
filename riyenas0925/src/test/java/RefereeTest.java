4cdb697b49113f81bec89387e17f33fe48185d64
riyenas0925 <riyenas0925@gmail.com>
2022-07-10 20:11:32 +0900

    refactor: validateNumber 테스트 코드 수정

--- riyenas0925/src/test/java/RefereeTest.java
+++ riyenas0925/src/test/java/RefereeTest.java
@@ -1,36 +1,25 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class RefereeTest {
 
     @DisplayName("입력값이 3자리가 아니라면 IllegalArgumentException을 발생시킨다.")
-    @ParameterizedTest(name = "{index}: {arguments}")
-    @MethodSource("parametersProvider")
-    void validateNumber(List<Integer> input) {
+    @ParameterizedTest(name = "입력값: {0}일때 예외 발생")
+    @ValueSource(strings = {"12", "1234"})
+    void validateNumber(String input) {
         // given, when, that
         assertThatThrownBy(() -> {
-            Referee referee = new Referee(input);
+            Referee referee = new Referee(split(input));
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력값은 3자리여야 합니다.");
-    }
-
-    static Stream<Arguments> parametersProvider() {
-        return Stream.of(
-                arguments(Arrays.asList(1, 2, 3, 4)),
-                arguments(Arrays.asList(1, 2))
-        );
     }
 
     @DisplayName("심판은 같은 수가 같은 자리에 있으면 스트라이크임을 알려줘야 한다.")
